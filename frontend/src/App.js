fc309ca7b20793f49f9b708dac90e968ade3fbf2
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-06 10:59:10 +0900

    #1. HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
+import { Container } from "react-bootstrap";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Container } from "react-bootstrap";
+
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -8,7 +11,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
