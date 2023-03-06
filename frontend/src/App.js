f92af719dfe3a2fb0d8c25998f249ac59579e0db
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-06 10:17:20 +0900

    #1. Reach-Bootstrap Setup, Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import logo from "./logo.svg";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
-  return <div>My App</div>;
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
